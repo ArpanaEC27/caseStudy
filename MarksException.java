a5066fb1b9121ae2bb2f956bf14a31354074523d
ArpanaEC27 <95286915+ArpanaEC27@users.noreply.github.com>
2022-09-26 16:29:34 +0530

    Add files via upload

---
+++ MarksException.java
@@ -0,0 +1,11 @@
+package com.gl.CaseStudies1;
+
+public class MarksException extends RuntimeException {
+	static final long serialVersionUID=1L;
+
+	public MarksException(String message) {
+		super(message);
+		
+	}
+
+}
